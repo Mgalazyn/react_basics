d39027820d5813590d3799f76dc5ad727e60c74b
Marcin Gałażyn <marcin.galazyn@itsquad.pl>
2025-07-07 21:27:46 +0200

    alert discloser

--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -1,11 +1,27 @@
 import { ReactNode } from "react";
+import { useState } from "react";
 
 interface AlertProps {
   children: ReactNode;
 }
 
 const Alert = ({ children }: AlertProps) => {
-  return <div className="alert alert-info">{children}</div>;
+  const [show, setShow] = useState(true);
+
+  if (!show) return null;
+
+  return (
+    <div className="alert alert-info alert-dismissible fade show" role="alert">
+      {children}
+      <button
+        type="button"
+        className="btn-close"
+        data-bs-dismiss="aler"
+        aria-label="Close"
+        onClick={() => setShow(false)}
+      ></button>
+    </div>
+  );
 };
 
 export default Alert;
